dc486dc02dea478fd636eff1d6746ca8264784f2
hongjoonchew <hongjoon.chew@gmail.com>
2016-12-01 20:51:04 -0800

    Reedited. Might cause huge issues

--- cs151.hw7/src/cs151/hw7/Canvas.java
+++ cs151.hw7/src/cs151/hw7/Canvas.java
@@ -1,7 +1,15 @@
 package cs151.hw7;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 public class Canvas extends JPanel{
+	public Canvas() {
+		super();
+		this.setPreferredSize(new Dimension(400,400));
+		this.setBackground(Color.WHITE);
+	}
 
 }
--- cs151.hw7/src/cs151/hw7/Whiteboard.java
+++ cs151.hw7/src/cs151/hw7/Whiteboard.java
@@ -1,5 +1,19 @@
 package cs151.hw7;
 
-public class Whiteboard {
+import java.awt.*;
 
+import javax.swing.JFrame;
+
+public class Whiteboard extends JFrame{
+	BorderLayout border = new BorderLayout();
+	public Whiteboard(){
+		super("Whiteboard");
+		this.setLayout(border);
+		this.add(new Canvas(),BorderLayout.CENTER);
+		this.setVisible(true);
+	}
+	
+	public static void main(String[] args){
+		Canvas c = new Canvas();
+	}
 }
