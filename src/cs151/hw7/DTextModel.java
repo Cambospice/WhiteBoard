342810455de07b2494c12d10649544a3cd6d8d3a
Cambospice <cambospice3@gmail.com>
2016-12-05 17:30:21 -0800

    Add files via upload
    updated DtextModel

--- src/cs151/hw7/DTextModel.java
+++ src/cs151/hw7/DTextModel.java
@@ -1,5 +1,37 @@
 package cs151.hw7;
 
-public class DTextModel {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
+public class DTextModel extends DShapeModel {
+	private String defaultWord = "Hello";
+	private String defaultFont = "Dialog";
+	private String theWord;
+	private String theFont;
+ 	public DTextModel() {
+		super();
+		theWord = defaultWord;
+		theFont = defaultFont;
+	}
+ 	
+ 	public String getText()
+ 	{
+ 		return theWord;
+ 	}
+ 	
+ 	public String getFont()
+ 	{
+ 		return theFont;
+ 	}
+
+	public static void showKnobsIfSelected(Graphics g) {
+		if (DShape.isSelected) {
+			Rectangle[] knobs = getKnobs();
+			for (int i = 0; i < knobs.length; i++) {
+				g.setColor(Color.GRAY);
+				g.fillRect(knobs[i].x, knobs[i].y, knobs[i].width, knobs[i].height);
+			}
+		}
+	}
 }
