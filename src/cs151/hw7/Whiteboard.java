f83d347065abbc13bc49e2f1c9bd2d075ab65b16
Cambospice <cambospice3@gmail.com>
2016-12-06 16:46:24 -0800

    Text field display

--- src/cs151/hw7/Whiteboard.java
+++ src/cs151/hw7/Whiteboard.java
@@ -6,41 +6,45 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class Whiteboard extends JFrame{
+public class Whiteboard extends JFrame {
 	BorderLayout border = new BorderLayout();
 	Canvas canvas = new Canvas();
 	static JPanel controlPanel;
 	static JFrame boardFrame;
-	//static Canvas canvas;
+	// static Canvas canvas;
 
 	static String item;
 	static JTextField text;
-	public Whiteboard(){
+
+	public Whiteboard() {
 		super("Whiteboard");
 		this.setLayout(border);
-		this.setSize(800,400);
+		this.setSize(800, 400);
 		Box controls = controls();
 		Box color = color();
 		Box movement = movement();
+		Box text = text();
 		Box vertical = Box.createVerticalBox();
 		vertical.add(controls);
 		vertical.add(Box.createVerticalStrut(70));
 		vertical.add(color);
 		vertical.add(Box.createVerticalStrut(70));
+		vertical.add(text);
+		vertical.add(Box.createVerticalStrut(70));
 		vertical.add(movement);
 		vertical.add(Box.createVerticalStrut(70));
-		for (Component comp : vertical.getComponents()) { 
-			((JComponent)comp).setAlignmentX(Box.LEFT_ALIGNMENT);
-			}
-		this.add(vertical,BorderLayout.WEST);
-		this.setVisible(true);
+		for (Component comp : vertical.getComponents()) {
+			((JComponent) comp).setAlignmentX(Box.LEFT_ALIGNMENT);
 		}
-	
-	public Box controls(){
+		this.add(vertical, BorderLayout.WEST);
+		this.setVisible(true);
+	}
+
+	public Box controls() {
 		Box b = Box.createHorizontalBox();
 		JButton rect = new JButton("Rect");
-		rect.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		rect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				canvas.addShape(new DRect());
 				canvas.repaint();
 			}
@@ -48,17 +52,17 @@ public class Whiteboard extends JFrame{
 		b.add(rect);
 		b.add(Box.createHorizontalStrut(40));
 		JButton oval = new JButton("Oval");
-		oval.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		oval.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				canvas.addShape(new DOval());
 				canvas.repaint();
 			}
 		});
 		b.add(oval);
 		b.add(Box.createHorizontalStrut(40));
-		JButton line  = new JButton("Line");
-		line.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		JButton line = new JButton("Line");
+		line.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				canvas.addShape(new DLine());
 				canvas.repaint();
 			}
@@ -66,67 +70,79 @@ public class Whiteboard extends JFrame{
 		b.add(line);
 		b.add(Box.createHorizontalStrut(40));
 		JButton text = new JButton("Text");
-		text.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		text.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				canvas.addShape(new DText());
 				canvas.repaint();
 			}
 		});
 		b.add(text);
-		this.add(canvas,BorderLayout.CENTER);
+		this.add(canvas, BorderLayout.CENTER);
 		return b;
 	}
-	
-	public Box color(){
+
+	public Box color() {
 		Box b = Box.createHorizontalBox();
 		JButton color = new JButton("Set Color");
-		color.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
+		color.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
 			}
 		});
 		b.add(color);
 		return b;
 	}
-	
-	public Box movement(){
+
+	public Box movement() {
 		Box b = Box.createHorizontalBox();
 		JButton moveToFront = new JButton("Move to Front");
-		moveToFront.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
+		moveToFront.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
 			}
 		});
 		b.add(moveToFront);
 		b.add(Box.createHorizontalStrut(40));
 		JButton moveToBack = new JButton("Move to Back");
-		moveToBack.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
+		moveToBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
 			}
 		});
 		b.add(moveToBack);
 		b.add(Box.createHorizontalStrut(40));
 		JButton remove = new JButton("Remove");
-		moveToFront.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				
+		moveToFront.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
 			}
 		});
 		b.add(remove);
+		
 		return b;
 	}
-	
-	public Box text(){ //not written
-		return null;
+
+	public Box text() { // not written
+		Box b = Box.createHorizontalBox();
+		b.add(Box.createHorizontalStrut(40));
+		text = new JTextField();
+		text.setMaximumSize(new Dimension(400, 500));
+		text.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				DTextModel.setText(text.getText());
+			}
+		});
+		b.add(text);
+		return b;
 	}
-	
-	public JTable table(){ //not written
+
+	public JTable table() { // not written
 		JTable table = new JTable();
 		return table;
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Whiteboard w = new Whiteboard();
 	}
 }
