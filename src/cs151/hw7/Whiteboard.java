2721aab28081ed8a4a6e2e9cec50924361066614
roshnivp <roshniknambiar@gmail.com>
2016-12-08 22:25:05 -0800

    server-client added, functionality need to be verify

--- src/cs151/hw7/Whiteboard.java
+++ src/cs151/hw7/Whiteboard.java
@@ -1,19 +1,27 @@
 package cs151.hw7;
 
+import sun.plugin2.message.Message;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.beans.XMLDecoder;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
 import javax.swing.*;
-import javax.swing.table.AbstractTableModel;
 
 public class Whiteboard extends JFrame {
 	BorderLayout border = new BorderLayout();
 	Canvas canvas = new Canvas();
+	JLabel status;
+	private ClientHandler clientHandler;
+	private ServerAccepter serverAccepter;
+	//ServerButtons serverButtons;
 
 	public Whiteboard() {
 		super("Whiteboard");
@@ -21,12 +29,14 @@ public class Whiteboard extends JFrame {
 		this.setSize(800, 400);
 		Box controls = controls();
 		Box color = color();
+		Box network=network();
 		Box movement = movement();
 		Box text = text();
 		Box vertical = Box.createVerticalBox();
 		vertical.add(controls);
 		vertical.add(Box.createVerticalStrut(70));
 		vertical.add(color);
+		vertical.add(network);
 		vertical.add(Box.createVerticalStrut(70));
 		vertical.add(text);
 		vertical.add(Box.createVerticalStrut(70));
@@ -40,6 +50,7 @@ public class Whiteboard extends JFrame {
 	}
 
 	public Box controls() {
+		//ListenForButton listenForButton = new ListenForButton();
 		Box b = Box.createHorizontalBox();
 		Box vb = Box.createVerticalBox();
 		JButton rect = new JButton("Rect");
@@ -85,6 +96,32 @@ public class Whiteboard extends JFrame {
 		return b;
 	}
 
+	public Box network() {
+		Box b = Box.createHorizontalBox();
+		JButton button;
+		button = new JButton("Start Server");
+		b.add(button);
+		button.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				doServer();
+			}
+		});
+
+		button = new JButton("Start Client");
+		b.add(button);
+		button.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				doClient();
+			}
+		});
+
+		status = new JLabel();
+		b.add(status);
+
+		//this.add(canvas, BorderLayout.CENTER);
+		return b;
+	}
+
 	public Box color() {
 		Box b = Box.createHorizontalBox();
 		JButton color = new JButton("Set Color");
@@ -100,10 +137,34 @@ public class Whiteboard extends JFrame {
 					canvas.repaint();
 			}
 		});
+
+
 		b.add(color);
 		return b;
 	}
-
+	// Starts the sever accepter to catch incoming client connections.
+	// Wired to Server button.
+	public void doServer() {
+		status.setText("Start server");
+		String result = JOptionPane.showInputDialog("Run server on port", "8001");
+		if (result!=null) {
+			System.out.println("server: start");
+			serverAccepter = new ServerAccepter(Integer.parseInt(result.trim()));
+			serverAccepter.start();
+		}
+	}
+	// Runs a client handler to connect to a server.
+	// Wired to Client button.
+	public void doClient() {
+		status.setText("Start client");
+		String result = JOptionPane.showInputDialog("Connect to host:port", "127.0.0.1:8001");
+		if (result!=null) {
+			String[] parts = result.split(":");
+			System.out.println("client: start");
+			clientHandler = new ClientHandler(parts[0].trim(), Integer.parseInt(parts[1].trim()));
+			clientHandler.start();
+		}
+	}
 	public Box movement() {
 		Box b = Box.createHorizontalBox();
 		JButton moveToFront = new JButton("Move to Front");
@@ -173,8 +234,6 @@ public class Whiteboard extends JFrame {
 		return combinedBox;
 	}
 	
-	
-	
 	public void saveImage(File file) {
 		// Create an image bitmap, same size as ourselves
 		BufferedImage image = (BufferedImage) createImage(getWidth(), getHeight());
@@ -192,36 +251,86 @@ public class Whiteboard extends JFrame {
 		}
 	}
 
-	public JTable table(){
-		String[] columnNames = { "x","y", "width", "height"};
-		JScrollPane scroller = new JScrollPane();
-		scroller.setPreferredSize(new Dimension(300,400));
-		return null;
+	public JTable table() { // not written
+		JTable table = new JTable();
+		return table;
 	}
+	// Client runs this to handle incoming messages
+	// (our client only uses the inputstream of the connection)
+	private class ClientHandler extends Thread {
+		private String name;
+		private int port;
+
+		ClientHandler(String name, int port) {
+			this.name = name;
+			this.port = port;
+		}
+		// Connect to the server, loop getting messages
+		public void run() {
+			try {
+				// make connection to the server name/port
+				Socket toServer = new Socket(name, port);
+				// get input stream to read from server and wrap in object input stream
+				ObjectInputStream in = new ObjectInputStream(toServer.getInputStream());
+				System.out.println("client: connected!");
+				// we could do this if we wanted to write to server in addition
+				// to reading
+				// out = new ObjectOutputStream(toServer.getOutputStream());
+				while (true) {
+					// Get the xml string, decode to a Message object.
+					// Blocks in readObject(), waiting for server to send something.
+					String xmlString = (String) in.readObject();
+					XMLDecoder decoder = new XMLDecoder(new ByteArrayInputStream(xmlString.getBytes()));
+					Message message = (Message) decoder.readObject();
+
+					//System.out.println("client: read " + message);
+				//	invokeToGUI(message);
+				}
+			}
+			catch (Exception ex) { // IOException and ClassNotFoundException
+				ex.printStackTrace();
+			}
+			// Could null out client ptr.
+			// Note that exception breaks out of the while loop,
+			// thus ending the thread.
+		}
+	}
+	// (this and sendToOutputs() are synchronzied to avoid conflicts)
+	public synchronized void addOutput(ObjectOutputStream out) {
+		outputs.add(out);
+	}
+	private java.util.List<ObjectOutputStream> outputs =
+			new ArrayList<ObjectOutputStream>();
+
+	// Server thread accepts incoming client connections
+	class ServerAccepter extends Thread {
+		private int port;
+		ServerAccepter(int port) {
+			this.port = port;
+		}
+		public void run() {
+			try {
+				ServerSocket serverSocket = new ServerSocket(port);
+				while (true) {
+					Socket toClient = null;
+					// this blocks, waiting for a Socket to the client
+					toClient = serverSocket.accept();
+					System.out.println("server: got client");
+					// Get an output stream to the client, and add it to
+					// the list of outputs
+					// (our server only uses the output stream of the connection)
+					addOutput(new ObjectOutputStream(toClient.getOutputStream()));
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+
+
+	}
+
 	public static void main(String[] args) {
 		Whiteboard w = new Whiteboard();
-	}
-	
-	private class DTableModel extends AbstractTableModel{
-		
-
-		@Override
-		public int getRowCount() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public int getColumnCount() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public Object getValueAt(int rowIndex, int columnIndex) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
 	}
 }
